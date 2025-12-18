5bc199b72c27071c81cdeccc402dcb3ceab5d856
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-12-17 21:26:56 -0500

    insert

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:gastosappg14/db/db_notas.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          DbNotas dbnotas = DbNotas();
+          // dbnotas.initDatabase();
+          dbnotas.insertNota(
+            "Compras del mes",
+            "Comprar pan, queso pollo carne etc",
+          );
+        },
+      ),
+    );
   }
 }
